b36c6537dcf265f1e5aac1a0b8388cf4da35edd5
chanchal219 <chanchal092228@gmail.com>
2025-05-11 04:03:10 +0530

    modify css

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Otpgenerator} from './components/Otpgenerator'
-import {AutomaticOtpGenerator} from './components/AutomaticOtpGenerator'
-import {Trafficlight} from './components/Trafficlight'
-import Todolist from './components/Todolist'
-import {Counter} from './components/Counter'
-import {Accordion} from './components/Accordion'
+import './App.css';
+import { Otpgenerator } from './components/Otpgenerator';
+import { AutomaticOtpGenerator } from './components/AutomaticOtpGenerator';
+import { Trafficlight } from './components/Trafficlight';
+import Todolist from './components/Todolist';
+import { Counter } from './components/Counter';
+import { Accordion } from './components/Accordion';
 
 function App() {
-
-
   return (
-    <>
-     {/* <Otpgenerator/> */}
-     {/* <AutomaticOtpGenerator/> */}
-     {/* <Accordion/> */}
-     {/* <Counter/> */}
-     {/* <Todolist/> */}
-     {/* <Trafficlight/> */}
-
-    </>
-  )
+    <div className="grid-container">
+      <div className="grid-item"><Otpgenerator /></div>
+      <div className="grid-item"><AutomaticOtpGenerator /></div>
+      <div className="grid-item"><Accordion /></div>
+      <div className="grid-item"><Counter /></div>
+      <div className="grid-item"><Todolist /></div>
+      <div className="grid-item"><Trafficlight /></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
